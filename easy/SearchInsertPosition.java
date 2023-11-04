1ed01a02529f2c22357bd4b1a6f512cf8ff92b7b
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2023-11-04 09:41:41 -0300

    easy-algorithms: Add solution to problem 35 search insert position from the LeetCode site.

---
+++ easy/SearchInsertPosition.java
@@ -0,0 +1,57 @@
+/**
+ * LeetCode
+ * 
+ * SEARCH INSERT POSITION
+ * 
+ * Given a sorted array of distinct integers and a target value, return the index if the target is found.
+ * If not, return the index where it would be if it were inserted in order.
+ * 
+ * You must write an algorithm with O(log n) runtime complexity.
+ * 
+ * Example 1:
+ *  Input: nums = [1,3,5,6], target = 5
+ *  Output: 2
+ * 
+ * Example 2:
+ *  Input: nums = [1,3,5,6], target = 2
+ *  Output: 1
+ * 
+ * Example 3:
+ *  Input: nums = [1,3,5,6], target = 7
+ *  Output: 4
+ * 
+ * implementing binary search algorithm 
+ * 
+ * @version 0.0.1 Novembro/02/2023
+ * @author  LUCAS, Rosivaldo
+ */
+
+public class SearchInsertPosition {
+  
+  public static int searchInsert(final int[] nums, final int target) {
+    int l = -1;
+    int r = nums.length;
+
+    while (l < (r - 1)) {
+      int m = (l + r) / 2;
+      
+      if (nums[m] < target) {
+        l = m;
+      } else {
+        r = m;
+      }
+    }
+
+    return r;
+  }
+
+  public static void main(String[] args) {
+    int[] nums = new int[] {1, 3, 5, 6, 7, 9};
+    int target = 8;
+
+    int result = searchInsert(nums, target);
+  
+    System.out.println(result);
+  }
+
+}
