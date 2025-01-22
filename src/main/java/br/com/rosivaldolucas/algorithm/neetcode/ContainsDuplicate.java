d3851b78ce3e9633b6b9f26b084277a77500c645
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2025-01-22 19:29:41 -0300

    neetcode: add other solution to the problem Contains Duplicate

--- src/main/java/br/com/rosivaldolucas/algorithm/neetcode/ContainsDuplicate.java
+++ src/main/java/br/com/rosivaldolucas/algorithm/neetcode/ContainsDuplicate.java
@@ -1,17 +1,34 @@
 package br.com.rosivaldolucas.algorithm.neetcode;
 
+import java.util.HashSet;
+import java.util.Set;
+
 public class ContainsDuplicate {
 
     public static void main(String[] args) {
-        System.out.println(containsDuplicate(new int[]{1, 2, 3, 1}));
+        int[] numbers = {1, 2, 3, 4};
+
+        System.out.println(containsDuplicate_01(numbers));
+        System.out.println(containsDuplicate_02(numbers));
     }
 
-    public static boolean containsDuplicate(int[] nums) {
+    public static boolean containsDuplicate_01(int[] nums) {
         for (int i = 0; i < nums.length; i++) {
             for (int j = i + 1; j < nums.length; j++) {
                 if (nums[i] == nums[j]) {
                     return true;
                 }
+            }
+        }
+        return false;
+    }
+
+    public static boolean containsDuplicate_02(int[] nums) {
+        Set<Integer> set = new HashSet<>();
+        for (int i = 0; i < nums.length - 1; i++) {
+            set.add(nums[i]);
+            if (set.contains(nums[i + 1])) {
+                return true;
             }
         }
         return false;
