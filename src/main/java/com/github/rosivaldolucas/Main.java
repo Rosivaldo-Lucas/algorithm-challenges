4e8d4f0d8e76ecb04b682022bbf8cf0d2356d1c2
Rosivaldo Lucas <rosivaldosilva@eng.ci.ufpb.br>
2025-01-21 23:19:52 -0300

    configure new repository

--- src/main/java/com/github/rosivaldolucas/Main.java
+++
@@ -1,9 +0,0 @@
-package com.github.rosivaldolucas;
-
-public class Main {
-
-  public static void main(String[] args) {
-
-  }
-
-}
--- src/main/java/com/github/rosivaldolucas/easy/hackerrank/Java1DArray.java
+++
@@ -1,9 +0,0 @@
-package com.github.rosivaldolucas.easy.hackerrank;
-
-public class Java1DArray {
-
-  public static void main(String[] args) {
-
-  }
-
-}
